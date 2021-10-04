4ab76621faa80273aca52a2aee4c03fe0abb7961
dea1j <jdayo2012@gmail.com>
2021-10-04 12:11:14 +0100

    Initial commit

---
+++ src/main/java/com/dea1j/fleetMgt/FleetMgtApplication.java
@@ -0,0 +1,13 @@
+package com.dea1j.fleetMgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetMgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dea1j/fleetMgt/FleetMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dea1j.fleetMgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
