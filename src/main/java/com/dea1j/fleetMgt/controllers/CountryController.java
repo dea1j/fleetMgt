28dd577131a77d71ab51ad87321a68728eb56ca1
dea1j <jdayo2012@gmail.com>
2021-10-11 12:54:22 +0100

    update

--- src/main/java/com/dea1j/fleetMgt/controllers/CountryController.java
+++ src/main/java/com/dea1j/fleetMgt/controllers/CountryController.java
@@ -1,13 +1,24 @@
 package com.dea1j.fleetMgt.controllers;
 
+import com.dea1j.fleetMgt.models.Country;
+import com.dea1j.fleetMgt.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class CountryController {
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries() {
+    public String getCountries(Model model) {
+        List<Country> countryList = countryService.getCountries();
+
+        model.addAttribute("countries", countryList);
         return "country";
     }
 }
--- src/main/java/com/dea1j/fleetMgt/services/CountryService.java
+++ src/main/java/com/dea1j/fleetMgt/services/CountryService.java
@@ -1,7 +1,19 @@
 package com.dea1j.fleetMgt.services;
 
+import com.dea1j.fleetMgt.models.Country;
+import com.dea1j.fleetMgt.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getCountries() {
+        return countryRepository.findAll();
+    }
 }
